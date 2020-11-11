85f5709c9558cbab17867da91e63a40af37e77b7
killpop3770 <komstroj3@mail.ru>
2020-11-11 11:30:39 +0300

    v2.0

--- pdf_parser.py
+++ pdf_parser.py
@@ -3,61 +3,54 @@ import json
 import os
 from openpyxl import load_workbook
 
-# files = os.listdir(".")
-# print(files)
-# print("\n")
-
-#Добавить адреса
-
-files = {'1.pdf', #ГВС
-			#'2.pdf', #ЦО
-			'3.pdf', #ГВС
-			#'4.pdf', #ЦО
-			'5.pdf', #ГВС
-			#'6.pdf', #ЦО
-			'7.pdf'} #ГВС
-
-row = 0
+directory = "reports"
+wb = load_workbook('./first.xlsx')
+sheet = wb['Лист1']
+files = os.listdir(os.path.abspath(directory))
 
 for file in files:
-	row+=1
-	print("File number: " +str(row))
-
+	os.chdir(directory)
 	tables = camelot.read_pdf(file)
+	os.chdir("..")
+	tables.export('foo.json', f='json', compress=False)
 
-	tables.export('foo.json', f='json', compress=False) # json, excel, html
-
-	wb = load_workbook('./first.xlsx')
-	sheet = wb['Лист1']
+	string = " ".join(file.split("_")[:-4])
+	string = string.replace("д. ", "д.")
+	string = str(string.replace("к. ", "корп.")[5:])
+	print("File name: " +string)
 
 	with open('foo-page-1-table-1.json') as report:
-		data = json.loads(report.read())
+		data = json.loads(report.read()) 
 		count = 0
 
 		# TWO ALTERNATIVES FOR ANALYZE
 		# for j in range(11):
 		# 	if "tпод" in data[0].get(str(j)):
 
-		
-		print("Часы наработки: "+str(data[-1].get('10'))+"\n")
+		for nameCell in range(3, sheet.max_row):
+			address = str(sheet[f'B{nameCell}'].value)
+			if string == address:
+				print("Status: ok!")
 
-		print("Средняя температура подачи: "+str(data[-1].get('5'))+"\n")
+				print("Operating hours: "+str(data[-1].get('10')))
 
-		for i in range(2, len(data)-1):
-			T = data[i].get('5')
-			Tflow = T.replace(',', '.')
-			if float(Tflow) < 60.0:
-				count+=1
-		print("Дней с температурой менее 60.0: "+str(count)+"\n")
+				print("Average Tflow: "+str(data[-1].get('5')))
 
-		position = str(row)
+				for i in range(2, len(data)-1):
+					T = data[i].get('5')
+					Tflow = T.replace(',', '.')
+					if float(Tflow) < 60.0:
+						count+=1
+				print("Days with Tflow before 60.0: "+str(count)+"\n")
 
-		Hours = sheet[f"B{position}"]
-		Tflow = sheet[f"C{position}"]
-		Days = sheet[f"D{position}"]
+				position = str(nameCell)
 
-		Hours.value = data[-1].get('10')
-		Tflow.value = data[-1].get('5')
-		Days.value = count
+				hours = sheet[f"D{position}"]
+				Tflow = sheet[f"E{position}"]
+				days = sheet[f"F{position}"]
 
-		wb.save("first.xlsx")
+				hours.value = data[-1].get('10')
+				Tflow.value = data[-1].get('5')
+				days.value = count
+
+	wb.save("first.xlsx")

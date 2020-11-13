dab37434a5a732d0334e0015e46ff5c418bcec7e
killpop3770 <komstroj3@mail.ru>
2020-11-13 12:37:09 +0300

    v2.1

--- pdf_parser.py
+++ pdf_parser.py
@@ -2,14 +2,26 @@ import camelot
 import json
 import os
 from openpyxl import load_workbook
+import time
+
+startTime = time.time()
 
 directory = "reports"
+absDirectory = os.path.abspath(directory)
+files = os.listdir(absDirectory)
+
 wb = load_workbook('./first.xlsx')
 sheet = wb['Лист1']
-files = os.listdir(os.path.abspath(directory))
+
+mode = 'a' if os.path.exists('storage_log.txt') else 'w'
+file = open('storage_log.txt', mode)
 
 for file in files:
-	os.chdir(directory)
+	os.chdir(absDirectory)
+	
+	if "ЦО" in file or "ХВС" in file:
+		continue
+
 	tables = camelot.read_pdf(file)
 	os.chdir("..")
 	tables.export('foo.json', f='json', compress=False)
@@ -17,40 +29,48 @@ for file in files:
 	string = " ".join(file.split("_")[:-4])
 	string = string.replace("д. ", "д.")
 	string = str(string.replace("к. ", "корп.")[5:])
-	print("File name: " +string)
+	print("File name: "+string)
 
-	with open('foo-page-1-table-1.json') as report:
-		data = json.loads(report.read()) 
-		count = 0
+	with open('storage_log.txt', 'tr') as f:
+		for line in f:
+			if line == string+"\n":
+				continue
+			else:
+				for nameCell in range(3, sheet.max_row):
+					address = str(sheet[f'B{nameCell}'].value)
+					if string == address:
 
-		# TWO ALTERNATIVES FOR ANALYZE
-		# for j in range(11):
-		# 	if "tпод" in data[0].get(str(j)):
+							with open('foo-page-1-table-1.json') as report:
+								data = json.loads(report.read()) 
+								count = 0
 
-		for nameCell in range(3, sheet.max_row):
-			address = str(sheet[f'B{nameCell}'].value)
-			if string == address:
-				print("Status: ok!")
+								try:
+									for i in range(2, len(data)-1):
+										T = data[i].get('5')
+										if T != "" or T is not None:
+												Tflow = T.replace(',', '.')
+												if float(Tflow) < 60.0:
+													count+=1
+										else:
+											Tflow = 60.0
+								except Exception as e:
+									print("Bad data\n")
+								finally:
+									Tflow = 60.0
 
-				print("Operating hours: "+str(data[-1].get('10')))
+								position = str(nameCell)
 
-				print("Average Tflow: "+str(data[-1].get('5')))
+								hours = sheet[f"D{position}"]
+								Tflow = sheet[f"E{position}"]
+								days = sheet[f"F{position}"]
 
-				for i in range(2, len(data)-1):
-					T = data[i].get('5')
-					Tflow = T.replace(',', '.')
-					if float(Tflow) < 60.0:
-						count+=1
-				print("Days with Tflow before 60.0: "+str(count)+"\n")
+								hours.value = data[-1].get('10')
+								Tflow.value = data[-1].get('5')
+								days.value = count
 
-				position = str(nameCell)
+								wb.save("first.xlsx")
 
-				hours = sheet[f"D{position}"]
-				Tflow = sheet[f"E{position}"]
-				days = sheet[f"F{position}"]
+	with open('storage_log.txt', 'ta') as f:
+		f.write(f"{string}\n")
 
-				hours.value = data[-1].get('10')
-				Tflow.value = data[-1].get('5')
-				days.value = count
-
-	wb.save("first.xlsx")
+	print("RunTime: "+str(time.time()-startTime))
